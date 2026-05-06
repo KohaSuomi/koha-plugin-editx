dd1f44d6cb9f29413b50898332b5a3e803bbd8f9
Andrii Nugged <nugged@gmail.com>
2026-05-06 12:02:53 +0300

    Add EDItX mapping configuration

--- Koha/Plugin/Fi/KohaSuomi/Editx.pm
+++ Koha/Plugin/Fi/KohaSuomi/Editx.pm
@@ -5,7 +5,8 @@ use Modern::Perl;
 use base qw(Koha::Plugins::Base);
 ## We will also need to include any Koha libraries we want to access
 use C4::Context;
-use JSON::Validator::Schema::OpenAPIv2;
+use Koha::DateUtils qw(dt_from_string);
+use Text::CSV_XS;
 use utf8;
 ## Here we set our plugin version
 our $VERSION = "1.0.4";
@@ -78,6 +79,48 @@ sub uninstall() {
     my $dbh = C4::Context->dbh;
     $dbh->do("DELETE FROM plugin_data WHERE plugin_class='Koha::Plugin::Fi::KohaSuomi::Editx' AND plugin_key='next_barcode';");
     return 1;
+}
+
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+    my @messages;
+    my $saved;
+
+    $self->_install_or_upgrade_tables();
+
+    if ( $cgi->request_method eq 'POST' && $cgi->param('save') ) {
+        my $mapping_csv = $cgi->param('mapping_csv') // '';
+        my ( $rows, $parse_messages, $has_blocking_errors ) = $self->_parse_productform_mapping_csv($mapping_csv);
+        push @messages, @$parse_messages;
+
+        if ($has_blocking_errors) {
+            $self->_output_configure_page(
+                mapping_csv    => $mapping_csv,
+                messages       => \@messages,
+                saved          => 0,
+            );
+            return;
+        }
+
+        my $save_messages = $self->_save_productform_mappings($rows);
+        push @messages, @$save_messages;
+        if (@$save_messages) {
+            $self->_output_configure_page(
+                mapping_csv => $mapping_csv,
+                messages    => \@messages,
+                saved       => 0,
+            );
+            return;
+        }
+        $saved = 1;
+    }
+
+    $self->_output_configure_page(
+        mapping_csv => $self->_productform_mapping_csv(),
+        messages    => \@messages,
+        saved       => $saved,
+    );
 }
 
 sub api_routes {
@@ -153,159 +196,232 @@ sub create_aqbudgets_spend_log {
     ");
 };
 
-sub sql_insert_data {
-    my ($self) = @_;
+sub _output_configure_page {
+    my ( $self, %params ) = @_;
+
+    my $template = $self->get_template( { file => 'configure.tt' } );
+    $template->param(
+        mapping_csv    => $params{mapping_csv},
+        messages       => $params{messages},
+        saved          => $params{saved},
+        itemtypes_text => join( ', ', @{ $self->_itemtypes() } ),
+        last_upgraded  => $self->retrieve_data('last_upgraded'),
+    );
+
+    return $self->output_html( $template->output() );
+}
+
+sub _parse_productform_mapping_csv {
+    my ( $self, $mapping_csv ) = @_;
+
+    my $csv = Text::CSV_XS->new(
+        {
+            binary           => 1,
+            allow_whitespace => 1,
+            blank_is_undef   => 1,
+        }
+    );
+    my %itemtypes = map { $_ => 1 } @{ $self->_itemtypes() };
+    my ( @rows, @messages, %seen_onix_codes );
+    my $has_blocking_errors;
+    my $line_number = 0;
+
+    open my $fh, '<', \$mapping_csv or die "Cannot read product form mapping CSV: $!";
+
+    while ( my $fields = $csv->getline($fh) ) {
+        $line_number++;
+        next unless grep { defined $_ && $_ ne '' } @$fields;
+        next if $line_number == 1 && $self->_is_productform_mapping_csv_header($fields);
+
+        if ( @$fields != 3 ) {
+            push @messages, $self->_configure_message( error => "Line $line_number has " . scalar(@$fields) . " columns; expected 3." );
+            $has_blocking_errors = 1;
+            next;
+        }
+
+        my ( $onix_code, $productform, $productform_alternative ) = map { $self->_trim_csv_value($_) } @$fields;
+
+        unless ($onix_code) {
+            push @messages, $self->_configure_message( error => "Line $line_number has no ONIX code." );
+            $has_blocking_errors = 1;
+            next;
+        }
+
+        if ( $seen_onix_codes{$onix_code}++ ) {
+            push @messages, $self->_configure_message( warning => "Line $line_number repeats ONIX code '$onix_code'; the later value will win." );
+        }
+
+        if ( $productform && !$itemtypes{$productform} ) {
+            push @messages, $self->_configure_message( warning => "Line $line_number: item type '$productform' does not exist; productform was stored as NULL." );
+            $productform = undef;
+        }
+
+        if ( $productform_alternative && !$itemtypes{$productform_alternative} ) {
+            push @messages, $self->_configure_message( warning => "Line $line_number: item type '$productform_alternative' does not exist; productform_alternative was stored as NULL." );
+            $productform_alternative = undef;
+        }
+
+        push @rows,
+            {
+                onix_code               => $onix_code,
+                productform             => $productform,
+                productform_alternative => $productform_alternative,
+            };
+    }
+
+    if ( !$csv->eof ) {
+        my ( $code, $message, $position ) = $csv->error_diag();
+        push @messages, $self->_configure_message( error => "CSV parse failed at line $line_number, position $position: $message ($code)." );
+        $has_blocking_errors = 1;
+    }
+
+    close $fh;
+
+    unless (@rows) {
+        push @messages, $self->_configure_message( error => 'No product form mappings found in CSV.' );
+        $has_blocking_errors = 1;
+    }
+
+    return ( \@rows, \@messages, $has_blocking_errors );
+}
+
+sub _save_productform_mappings {
+    my ( $self, $rows ) = @_;
+
     my $dbh = C4::Context->dbh;
-    $dbh->do("INSERT INTO map_productform (onix_code, productform, productform_alternative) VALUES
-        ('AA', '28VRK', '28VRKLN'),
-        ('AB', '28VRKAV', '28VRKLNAV'),
-        ('AC', '28VRKAV', '28VRKLNAV'),
-        ('AD', '28VRKAV', '28VRKLNAV'),
-        ('AE', '28VRK', '28VRKLN'),
-        ('AF', '28VRK', '28VRKLN'),
-        ('AG', '28VRK', '28VRKLN'),
-        ('AH', '28VRK', '28VRKLN'),
-        ('AI', '28VRKAV', '28VRKLNAV'),
-        ('AJ', '28VRK', '28VRKLN'),
-        ('AK', '28VRK', '28VRKLN'),
-        ('AL', '28VRK', '28VRKLN'),
-        ('AM', '28VRK', '28VRKLN'),
-        ('AZ', '28VRK', '28VRKLN'),
-        ('BA', '28VRK', '28VRKLN'),
-        ('BB', '28VRK', '28VRKLN'),
-        ('BC', '28VRK', '28VRKLN'),
-        ('BD', '28VRK', '28VRKLN'),
-        ('BE', '28VRK', '28VRKLN'),
-        ('BF', '28VRK', '28VRKLN'),
-        ('BG', '28VRK', '28VRKLN'),
-        ('BH', '28VRK', '28VRKLN'),
-        ('BI', '28VRK', '28VRKLN'),
-        ('BJ', '28VRK', '28VRKLN'),
-        ('BK', '28VRK', '28VRKLN'),
-        ('BL', '28VRK', '28VRKLN'),
-        ('BM', '28VRK', '28VRKLN'),
-        ('BN', '28VRK', '28VRKLN'),
-        ('BO', '28VRK', '28VRKLN'),
-        ('BP', '28VRK', '28VRKLN'),
-        ('BZ', '28VRK', '28VRKLN'),
-        ('CA', '28VRK', '28VRKLN'),
-        ('CB', '28VRK', '28VRKLN'),
-        ('CC', '28VRK', '28VRKLN'),
-        ('CD', '28VRK', '28VRKLN'),
-        ('CE', '28VRK', '28VRKLN'),
-        ('CZ', '28VRK', '28VRKLN'),
-        ('DA', '28VRK', '28VRKLN'),
-        ('DB', '28VRKAV', '28VRKLNAV'),
-        ('DC', '28VRKAV', '28VRKLNAV'),
-        ('DD', '14VRK', '14VRKLN'),
-        ('DE', '14VRK2LA', '14VRKLN2LA'),
-        ('DF', '28VRK', '28VRKLN'),
-        ('DG', 'EILAINATA', 'EILAINATA'),
-        ('DH', 'EILAINATA', 'EILAINATA'),
-        ('DI', '28VRK', '28VRKLN'),
-        ('DJ', '28VRK', '28VRKLN'),
-        ('DK', '28VRK', '28VRKLN'),
-        ('DL', '28VRK', '28VRKLN'),
-        ('DM', '28VRK', '28VRKLN'),
-        ('DN', '28VRK', '28VRKLN'),
-        ('DO', '28VRK', '28VRKLN'),
-        ('DZ', '28VRK', '28VRKLN'),
-        ('FA', '28VRK', '28VRKLN'),
-        ('FB', '28VRK', '28VRKLN'),
-        ('FC', '28VRK', '28VRKLN'),
-        ('FD', '28VRK', '28VRKLN'),
-        ('FE', '28VRK', '28VRKLN'),
-        ('FF', '28VRK', '28VRKLN'),
-        ('FZ', '28VRK', '28VRKLN'),
-        ('MA', '28VRK', '28VRKLN'),
-        ('MB', '28VRK', '28VRKLN'),
-        ('MC', '28VRK', '28VRKLN'),
-        ('MZ', '28VRK', '28VRKLN'),
-        ('PA', '28VRK', '28VRKLN'),
-        ('PB', '28VRK', '28VRKLN'),
-        ('PC', '28VRK', '28VRKLN'),
-        ('PD', '28VRK', '28VRKLN'),
-        ('PE', '28VRK', '28VRKLN'),
-        ('PF', '28VRK', '28VRKLN'),
-        ('PG', '28VRK', '28VRKLN'),
-        ('PH', '28VRK', '28VRKLN'),
-        ('PI', '28VRK', '28VRKLN'),
-        ('PJ', '28VRK', '28VRKLN'),
-        ('PK', '28VRK', '28VRKLN'),
-        ('PL', '28VRK', '28VRKLN'),
-        ('PM', '28VRK', '28VRKLN'),
-        ('PN', '28VRK', '28VRKLN'),
-        ('PO', '28VRK', '28VRKLN'),
-        ('PP', '28VRK', '28VRKLN'),
-        ('PQ', '28VRK', '28VRKLN'),
-        ('PR', '28VRK', '28VRKLN'),
-        ('PS', '28VRK', '28VRKLN'),
-        ('PT', '28VRK', '28VRKLN'),
-        ('PU', '28VRK', '28VRKLN'),
-        ('PV', '28VRK', '28VRKLN'),
-        ('PZ', '28VRK', '28VRKLN'),
-        ('SA', '28VRK', '28VRKLN'),
-        ('SB', '28VRK', '28VRKLN'),
-        ('SC', '28VRK', '28VRKLN'),
-        ('SD', '28VRK', '28VRKLN'),
-        ('SE', '28VRK', '28VRKLN'),
-        ('SF', '28VRK', '28VRKLN'),
-        ('VA', '28VRKAV', '28VRKLNAV'),
-        ('VB', '28VRKAV', '28VRKLNAV'),
-        ('VC', '28VRKAV', '28VRKLNAV'),
-        ('VD', '28VRKAV', '28VRKLNAV'),
-        ('VE', '28VRKAV', '28VRKLNAV'),
-        ('VF', '28VRK', '28VRKLN'),
-        ('VG', '28VRKAV', '28VRKLNAV'),
-        ('VH', '28VRKAV', '28VRKLNAV'),
-        ('VI', '14VRK', '14VRKLN'),
-        ('VJ', '28VRKAV', '28VRKLNAV'),
-        ('VK', '28VRK', '28VRKLN'),
-        ('VL', '28VRK', '28VRKLN'),
-        ('VM', '28VRK', '28VRKLN'),
-        ('VN', '28VRK', '28VRKLN'),
-        ('VO', '14VRK', '14VRKLN'),
-        ('VP', '28VRK', '28VRKLN'),
-        ('VQ', '28VRKAV', '28VRKLNAV'),
-        ('VZ', '28VRK', '28VRKLN'),
-        ('WW', '28VRK', '28VRKLN'),
-        ('WX', '28VRK', '28VRKLN'),
-        ('XA', '28VRK', '28VRKLN'),
-        ('XB', '28VRK', '28VRKLN'),
-        ('XC', '28VRK', '28VRKLN'),
-        ('XD', '28VRK', '28VRKLN'),
-        ('XE', '28VRK', '28VRKLN'),
-        ('XF', '28VRK', '28VRKLN'),
-        ('XG', '28VRK', '28VRKLN'),
-        ('XH', '28VRK', '28VRKLN'),
-        ('XI', '28VRK', '28VRKLN'),
-        ('XJ', '28VRK', '28VRKLN'),
-        ('XK', '28VRK', '28VRKLN'),
-        ('XL', '28VRK', '28VRKLN'),
-        ('XM', '28VRK', '28VRKLN'),
-        ('XN', '28VRK', '28VRKLN'),
-        ('XO', '28VRK', '28VRKLN'),
-        ('XZ', '28VRK', '28VRKLN'),
-        ('ZA', '28VRK', '28VRKLN'),
-        ('ZB', '28VRK', '28VRKLN'),
-        ('ZC', '28VRK', '28VRKLN'),
-        ('ZD', '28VRK', '28VRKLN'),
-        ('ZE', '14VRK', '14VRKLN'),
-        ('ZF', '28VRK', '28VRKLN'),
-        ('ZG', '28VRK', '28VRKLN'),
-        ('ZH', '28VRK', '28VRKLN'),
-        ('ZI', '28VRK', '28VRKLN'),
-        ('ZJ', '28VRK', '28VRKLN'),
-        ('ZK', '28VRK', '28VRKLN'),
-        ('ZL', '28VRK', '28VRKLN'),
-        ('ZM', '28VRK', '28VRKLN'),
-        ('ZN', '28VRK', '28VRKLN'),
-        ('ZX', '28VRK', '28VRKLN'),
-        ('ZY', '28VRK', '28VRKLN'),
-        ('ZZ', '28VRK', '28VRKLN'),
-        ('00', '28VRK', '28VRKLN');
-        ");    
-    }   
+    my $map_productform_table = 'map_productform';
+    my @messages;
+
+    my $saved = eval {
+        $dbh->begin_work;
+        $dbh->do("DELETE FROM $map_productform_table") or die $dbh->errstr;
+
+        my $sth = $dbh->prepare( "
+            INSERT INTO $map_productform_table (onix_code, productform, productform_alternative)
+            VALUES (?, ?, ?)
+            ON DUPLICATE KEY UPDATE
+                productform = VALUES(productform),
+                productform_alternative = VALUES(productform_alternative)
+        " );
+
+        for my $row (@$rows) {
+            $sth->execute( $row->{onix_code}, $row->{productform}, $row->{productform_alternative} ) or die $dbh->errstr;
+        }
+
+        $dbh->commit;
+        1;
+    };
+
+    if ( !$saved ) {
+        my $error = $@ || $dbh->errstr;
+        eval { $dbh->rollback };
+        push @messages, $self->_configure_message( error => "Could not save product form mappings: $error" );
+    }
+
+    return \@messages;
+}
+
+sub _productform_mapping_csv {
+    my ($self) = @_;
+
+    my $dbh = C4::Context->dbh;
+    my $map_productform_table = 'map_productform';
+    my $sth = $dbh->prepare( "
+        SELECT onix_code, productform, productform_alternative
+        FROM $map_productform_table
+        ORDER BY onix_code
+    " );
+    $sth->execute();
+
+    my $csv = Text::CSV_XS->new(
+        {
+            binary => 1,
+            eol    => "\n",
+        }
+    );
+    my $mapping_csv = '';
+
+    open my $fh, '>', \$mapping_csv or die "Cannot write product form mapping CSV: $!";
+    $csv->print( $fh, [qw(onix_code productform productform_alternative)] );
+
+    while ( my $row = $sth->fetchrow_hashref ) {
+        $csv->print(
+            $fh,
+            [
+                $row->{onix_code}               // '',
+                $row->{productform}             // '',
+                $row->{productform_alternative} // '',
+            ]
+        );
+    }
+
+    close $fh;
+
+    return $mapping_csv;
+}
+
+sub _itemtypes {
+    my ($self) = @_;
+
+    my $dbh = C4::Context->dbh;
+    my $sth = $dbh->prepare('SELECT itemtype FROM itemtypes ORDER BY itemtype');
+    $sth->execute();
+
+    my @itemtypes;
+    while ( my ($itemtype) = $sth->fetchrow_array ) {
+        push @itemtypes, $itemtype;
+    }
+
+    return \@itemtypes;
+}
+
+sub _is_productform_mapping_csv_header {
+    my ( $self, $fields ) = @_;
+
+    return unless @$fields == 3;
+
+    my @header = map { lc( $self->_trim_csv_value($_) // '' ) } @$fields;
+    return $header[0] eq 'onix_code'
+        && $header[1] eq 'productform'
+        && $header[2] eq 'productform_alternative';
+}
+
+sub _trim_csv_value {
+    my ( $self, $value ) = @_;
+
+    return unless defined $value;
+
+    $value =~ s/\A\x{FEFF}//;
+    $value =~ s/\A\s+|\s+\z//g;
+    return $value eq '' || uc($value) eq 'NULL' ? undef : $value;
+}
+
+sub _configure_message {
+    my ( $self, $type, $text ) = @_;
+
+    return {
+        type        => $type,
+        alert_class => $type eq 'error' ? 'danger' : $type,
+        text        => $text,
+    };
+}
+
+sub _table_exists {
+    my ( $self, $table_name ) = @_;
+
+    my $sth = C4::Context->dbh->prepare("SHOW TABLES LIKE ?");
+    $sth->execute($table_name);
+
+    return $sth->fetchrow_array ? 1 : 0;
+}
+
+sub _quote_identifier {
+    my ( $self, $identifier ) = @_;
+
+    $identifier =~ s/`/``/g;
+    return "`$identifier`";
+}
+
 1;
 
 
